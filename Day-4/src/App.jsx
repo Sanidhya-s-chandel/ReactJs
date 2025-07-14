b5afa28ce2a506062424adbbf9b178617329f033
Sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2025-07-14 12:19:38 +0530

    usderstanding the componests

---
+++ Day-4/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Greet from './Greet'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-black flex items-center justify-center'>
+      <Greet />
+    </div>
+  )
+}
+
+export default App
---
+++ Day-4/src/Greet.jsx
@@ -0,0 +1,9 @@
+function Greet(){
+    return(
+        <div className="bg-white w-[30%] font-semibold border rounded-lg flex flex-col items-center p-8">
+            <h1 className="text-center text-lg">Hello, This is from the component...</h1>
+        </div>
+    )
+};
+
+export default Greet;
